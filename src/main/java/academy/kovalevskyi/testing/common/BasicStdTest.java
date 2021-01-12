2334246ac92463e77d33ff7638f090aed53687c8
Vladyslav <ecstasy.awesome@gmail.com>
2021-01-12 15:42:01 +0200

    fixed problem with printing to console

--- src/main/java/academy/kovalevskyi/testing/common/BasicStdTest.java
+++ src/main/java/academy/kovalevskyi/testing/common/BasicStdTest.java
@@ -3,15 +3,22 @@ package academy.kovalevskyi.testing.common;
 import academy.kovalevskyi.testing.AbstractTestExecutor;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
 
 public abstract class BasicStdTest extends AbstractTestExecutor {
 
   protected final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+  private PrintStream printStream;
 
   @BeforeEach
-  public void setUp() {
+  public void setUpCustomOutput() {
+    printStream = System.out;
     System.setOut(new PrintStream(outputStreamCaptor));
+  }
+
+  @AfterEach
+  public void setUpDefaultOutput() {
+    System.setOut(printStream);
   }
 }

aea217d181a33e0f08cce955005b6442a3dc19f6
Vladyslav <ecstasy.awesome@gmail.com>
2021-01-18 18:28:20 +0200

    changed console output refactoring

--- src/main/java/academy/kovalevskyi/testing/view/TestsConsolePrinter.java
+++ src/main/java/academy/kovalevskyi/testing/view/TestsConsolePrinter.java
@@ -1,6 +1,8 @@
 package academy.kovalevskyi.testing.view;
 
+import java.util.StringJoiner;
 import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.Ansi.Color;
 import org.fusesource.jansi.AnsiConsole;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -11,78 +13,96 @@ public class TestsConsolePrinter implements TestWatcher, BeforeAllCallback, Afte
 
   private int successful = 0;
   private int failed = 0;
-  private boolean isNoClass;
-  private static boolean isSilentMode;
+  private boolean noClassDef;
+  private static boolean silentMode;
 
-  public static void setSilentMode(boolean silentMode) {
-    isSilentMode = silentMode;
+  public static void setSilentMode(boolean silent) {
+    silentMode = silent;
   }
 
   @Override
-  public void beforeAll(ExtensionContext extensionContext) {
+  public void beforeAll(ExtensionContext context) {
     AnsiConsole.systemInstall();
-    if (!isSilentMode) {
-      System.out.printf("Result of %s\n\n", extensionContext.getDisplayName());
+    if (!silentMode) {
+      System.out.println(getHeader(context));
     }
   }
 
   @Override
   public void testSuccessful(ExtensionContext context) {
     successful++;
-    if (!isSilentMode) {
-      System.out.println(Ansi
-          .ansi()
-          .a(context.getDisplayName())
-          .a(" - ")
-          .fgGreen()
-          .a("OK")
-          .reset());
+    if (!silentMode) {
+      System.out.println(getEntry(context, "OK", Color.GREEN));
     }
   }
 
   @Override
   public void testFailed(ExtensionContext context, Throwable cause) {
+    if (silentMode && failed == 0) {
+      System.out.println(getHeader(context));
+    }
     failed++;
-    if (!isNoClass) {
-      if (cause instanceof NoClassDefFoundError) {
-        isNoClass = true;
-        System.out.println(Ansi
-            .ansi()
-            .fgRed()
-            .a("Class under test not found, very-very likely you have incorrectly set class path\n")
-            .a("or you have changed structure of the project ")
-            .a("so Zeus can not find the class below\n")
-            .a("'")
-            .a(cause.getMessage())
-            .a("'")
-            .reset());
-      } else {
-        var message = Ansi
-            .ansi()
-            .a(context.getDisplayName())
-            .a(" - ")
-            .fgRed()
-            .a("BAD\n\n");
-        if (cause instanceof NoSuchMethodError) {
-          message.a(String.format("'%s' is not exist, but should.", cause.getMessage()));
-        } else {
-          message.a(cause.getMessage());
-        }
-        message.a("\n\n").reset();
-        System.out.println(message);
-      }
+    if (!noClassDef) {
+      System.out.println(getEntry(context, cause, "BAD", Color.RED));
     }
   }
 
   @Override
-  public void afterAll(ExtensionContext extensionContext) {
-    if (!isSilentMode) {
-      System.out.printf("\nTotal: %d\n", successful + failed);
-      System.out.printf("Successful: %d\n", successful);
-      System.out.printf("Failed : %d\n\n", failed);
+  public void afterAll(ExtensionContext context) {
+    if (!silentMode) {
+      System.out.printf("TOTAL %d | SUCCESSFUL %d | FAILED %d%n%n",
+          successful + failed,
+          successful,
+          failed);
     } else if (failed == 0) {
-      System.out.printf("%s is done successfully!\n", extensionContext.getDisplayName());
+      System.out.printf("%s is done successfully!%n", context.getDisplayName());
+    } else {
+      System.out.printf("You have %d failed method(s)%n%n", failed);
     }
     AnsiConsole.systemUninstall();
   }
+
+  private String getHeader(ExtensionContext context) {
+    return String.format("Result of %s:", context.getDisplayName());
+  }
+
+  private String getEntry(ExtensionContext context, String status, Color color) {
+    return Ansi
+        .ansi()
+        .format("%s - ", context.getDisplayName())
+        .fg(color)
+        .a(status)
+        .reset()
+        .toString();
+  }
+
+  private String getEntry(ExtensionContext context, Throwable cause, String status, Color color) {
+    if (cause instanceof NoClassDefFoundError) {
+      noClassDef = true;
+      return Ansi
+          .ansi()
+          .fg(color)
+          .format("Zeus can not find '%s'%n", cause.getMessage())
+          .a("Reasons:")
+          .newline()
+          .a("- your jar file is absent in the classpath")
+          .newline()
+          .a("- class is absent")
+          .newline()
+          .a("- structure of the project is not default")
+          .reset()
+          .toString();
+    }
+    var result = new StringJoiner(System.lineSeparator());
+    result.add(getEntry(context, status, color));
+    var message = Ansi.ansi().fg(color);
+    if (cause instanceof NoSuchMethodError) {
+      message.format("'%s' is absent, write the method", cause.getMessage());
+    } else {
+      message.a(cause.getMessage());
+    }
+    result.add(message.reset().toString());
+    return result.toString();
+  }
+
 }

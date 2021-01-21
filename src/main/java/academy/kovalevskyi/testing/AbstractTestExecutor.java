61b8583a92336f4c8a717f87aa90d31bbe93f395
Vladyslav <ecstasy.awesome@gmail.com>
2021-01-22 00:36:24 +0200

    added timeout for each test

--- src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
+++ src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
@@ -3,12 +3,14 @@ package academy.kovalevskyi.testing;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 import academy.kovalevskyi.testing.view.TestsConsolePrinter;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
+@Timeout(value = 7)
 @ExtendWith(TestsConsolePrinter.class)
 public abstract class AbstractTestExecutor {
 

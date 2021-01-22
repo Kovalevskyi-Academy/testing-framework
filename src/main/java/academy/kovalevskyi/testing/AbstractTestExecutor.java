c559f98928d36103c4fb7d0469fe5c80b167346f
Vladyslav <ecstasy.awesome@gmail.com>
2021-01-22 02:50:36 +0200

    Timeout,refactoring
    -removed junit timeout annotation
    -added global timeout variable to each test
    -minor fixes

--- src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
+++ src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
@@ -3,28 +3,26 @@ package academy.kovalevskyi.testing;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 import academy.kovalevskyi.testing.view.TestsConsolePrinter;
-import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
-@Timeout(value = 7)
 @ExtendWith(TestsConsolePrinter.class)
 public abstract class AbstractTestExecutor {
 
-  private final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+  public static final int TEST_TIMEOUT_SEC = 10;
 
   /**
-   * Execute test class programmatically.
+   * Launch JUnit 5 and execute test class programmatically.
    */
   public void executeTest() {
+    var listener = new SummaryGeneratingListener();
     var request = LauncherDiscoveryRequestBuilder
         .request()
         .selectors(selectClass(this.getClass()))
         .build();
-    Launcher launcher = LauncherFactory.create();
+    var launcher = LauncherFactory.create();
     launcher.discover(request);
     launcher.registerTestExecutionListeners(listener);
     launcher.execute(request);

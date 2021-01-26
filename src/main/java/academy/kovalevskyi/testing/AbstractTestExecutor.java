32d7f1dd891d33a8c6efb70da3917179eacdc677
Vladyslav <ecstasy.awesome@gmail.com>
2021-01-26 02:20:11 +0200

    refactoring renamed executeTest() to execute() deleted timeout variable (moved to TestHandler.java)

--- src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
+++ src/main/java/academy/kovalevskyi/testing/AbstractTestExecutor.java
@@ -2,36 +2,30 @@ package academy.kovalevskyi.testing;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
-import academy.kovalevskyi.testing.view.TestsConsolePrinter;
+import academy.kovalevskyi.testing.view.TestHandler;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
-
 /**
- * This class allows you to apply some settings and parameters globally for all tests.
- * All tests directly or indirectly need to inherit from this class.
- *
- * @ExtendWith — ??.
+ * This class allows you to invoke test classes programmatically. All test classes directly or
+ * indirectly need to inherit from this class.
  */
-@ExtendWith(TestsConsolePrinter.class)
-// @Timeout(value = 5, unit = TimeUnit.SECONDS)
+
+@ExtendWith(TestHandler.class)
 public abstract class AbstractTestExecutor {
 
-  public static final int TEST_TIMEOUT_SEC = 20;
-
-
   /**
-   * Launch JUnit 5 and execute test class programmatically.
+   * Launch JUnit 5 and invoke test class programmatically.
    */
-  public void executeTest() {
-    var listener = new SummaryGeneratingListener();
-    var request = LauncherDiscoveryRequestBuilder
+  public void execute() {
+    final var listener = new SummaryGeneratingListener();
+    final var request = LauncherDiscoveryRequestBuilder
         .request()
         .selectors(selectClass(this.getClass()))
         .build();
-    var launcher = LauncherFactory.create();
+    final var launcher = LauncherFactory.create();
     launcher.discover(request);
     launcher.registerTestExecutionListeners(listener);
     launcher.execute(request);

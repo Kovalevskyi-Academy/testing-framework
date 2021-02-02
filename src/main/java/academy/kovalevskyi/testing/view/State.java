25721a6a4bfac56b5d622fa3918ed350d662665d
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-03 00:38:34 +0200

    add some enum set of states of a test

---
+++ src/main/java/academy/kovalevskyi/testing/view/State.java
@@ -0,0 +1,25 @@
+package academy.kovalevskyi.testing.view;
+
+import org.fusesource.jansi.Ansi.Color;
+
+/**
+ * Set of states of a test
+ */
+public enum State {
+
+  SUCCESSFUL("OK", Color.GREEN),
+  FAILED("BAD", Color.RED),
+  ABORTED("NONE", Color.YELLOW),
+  RUNNING("RUN", Color.BLUE),
+  INTERRUPTED("FATAL", Color.RED),
+  NO_METHOD("ERROR", Color.RED),
+  NO_CLASS("ERROR", Color.RED);
+
+  public final String status;
+  public final Color color;
+
+  State(String status, Color color) {
+    this.status = status;
+    this.color = color;
+  }
+}

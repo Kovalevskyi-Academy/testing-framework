e05504905b1e775998cf8701694963d755830e1a
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-03 00:36:12 +0200

    add some modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module academy.kovalevskyi.testing {
-    exports academy.kovalevskyi.testing;
-    exports academy.kovalevskyi.testing.view;
-    exports academy.kovalevskyi.testing.common;
+  exports academy.kovalevskyi.testing;
+  exports academy.kovalevskyi.testing.view;
+  exports academy.kovalevskyi.testing.common;
 
-    requires org.junit.jupiter.api;
-    requires org.junit.vintage.engine;
-    requires org.junit.platform.launcher;
-    requires com.google.common;
-    requires org.fusesource.jansi;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.jupiter.params;
+  requires org.junit.vintage.engine;
+  requires org.junit.platform.launcher;
+  requires org.mockito;
+  requires com.google.common;
+  requires org.fusesource.jansi;
 }

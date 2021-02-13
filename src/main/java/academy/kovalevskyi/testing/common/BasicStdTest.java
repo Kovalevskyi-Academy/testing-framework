9ddc4e9c675f1315cddbc0853acb7da1f2c335f8
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-13 16:10:08 +0200

    refactoring

--- src/main/java/academy/kovalevskyi/testing/common/BasicStdTest.java
+++ src/main/java/academy/kovalevskyi/testing/common/BasicStdTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 /**
- * This class is required for all tests that are going to intercept what
- * will be displayed in the method under test.
- * Test classes with display interceptor methods must inherit from this abstract class.
+ * This class is required for all tests that are going to intercept what will be displayed in the
+ * method under test. Test classes with display interceptor methods must inherit from this abstract
+ * class.
  */
 public abstract class BasicStdTest extends AbstractTestExecutor {
 

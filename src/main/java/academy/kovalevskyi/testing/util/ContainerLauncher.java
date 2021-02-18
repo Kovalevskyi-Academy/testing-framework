7a0a6110cadc60cd6d60a7619e3edac7b066baae
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-18 18:37:45 +0200

    refactoring

--- src/main/java/academy/kovalevskyi/testing/util/ContainerLauncher.java
+++ src/main/java/academy/kovalevskyi/testing/util/ContainerLauncher.java
@@ -8,10 +8,9 @@ import academy.kovalevskyi.testing.service.ContainerRequest;
 import academy.kovalevskyi.testing.service.IFrameworkProperty;
 import academy.kovalevskyi.testing.service.State;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
+import org.junit.platform.commons.JUnitException;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 
@@ -22,9 +21,8 @@ public class ContainerLauncher {
    *
    * @param errorMode show only unsuccessful methods
    * @param container test class
-   * @throws Exception a lot of reasons
    */
-  public static void execute(final boolean errorMode, final Class<?> container) throws Exception {
+  public static void execute(final boolean errorMode, final Class<?> container) {
     setErrorMode(errorMode);
     execute(container);
   }
@@ -34,10 +32,8 @@ public class ContainerLauncher {
    *
    * @param errorMode  show only unsuccessful methods
    * @param containers list of test classes
-   * @throws Exception a lot of reasons
    */
-  public static void execute(final boolean errorMode, final List<Class<?>> containers)
-      throws Exception {
+  public static void execute(final boolean errorMode, final List<Class<?>> containers) {
     if (containers.isEmpty()) {
       throw new ContainerNotFoundException("No containers to execute");
     }
@@ -52,36 +48,35 @@ public class ContainerLauncher {
    *
    * @param errorMode show only unsuccessful methods
    * @param request   combined request of containers
-   * @throws Exception a lot of reasons
    */
-  public static void execute(final boolean errorMode, final ContainerRequest request)
-      throws Exception {
+  public static void execute(final boolean errorMode, final ContainerRequest request) {
     execute(errorMode, ContainerManager.getContainers(request));
   }
 
-  private static void execute(final Class<?> container) throws Exception {
+  private static void execute(final Class<?> container) {
     final var request = LauncherDiscoveryRequestBuilder
         .request()
         .selectors(selectClass(container))
         .build();
     try {
       LauncherFactory.create().execute(request);
-    } catch (Exception exception) {
-      final var noClassException = (NoClassDefFoundError) Stream
-          .iterate(exception, Objects::nonNull, Throwable::getCause)
-          .filter(throwable -> throwable.getClass().equals(NoClassDefFoundError.class))
-          .findFirst()
-          .orElseThrow(() -> exception);
-      AnsiConsole.systemInstall();
-      var message = Ansi
-          .ansi()
-          .fg(State.NO_CLASS.color)
-          .a(ContainerHandler.getReason(noClassException))
-          .reset()
-          .toString();
-      System.out.println(message);
-      System.out.println("-----------------------------------------");
-      AnsiConsole.systemUninstall();
+    } catch (JUnitException exception) {
+      final var noClassException =
+          ContainerHandler.getExceptionFromThrowableChain(exception, NoClassDefFoundError.class);
+      if (noClassException.isPresent()) {
+        AnsiConsole.systemInstall();
+        var message = Ansi
+            .ansi()
+            .fg(State.NO_CLASS.color)
+            .a(ContainerHandler.getReason((NoClassDefFoundError) noClassException.get()))
+            .reset()
+            .toString();
+        System.out.println(message);
+        System.out.println("-----------------------------------------");
+        AnsiConsole.systemUninstall();
+      } else {
+        throw exception;
+      }
     }
   }
 

072f3379336186b09302ab4086a2551d63f1aa1e
Vladyslav <ecstasy.awesome@gmail.com>
2021-03-05 11:45:39 +0200

    introduce a console handler to work in pair with debug mode

---
+++ src/main/java/academy/kovalevskyi/testing/service/StdConsoleHandler.java
@@ -0,0 +1,51 @@
+package academy.kovalevskyi.testing.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.Ansi.Color;
+
+public class StdConsoleHandler extends Thread {
+
+  private final ByteArrayOutputStream buffer;
+  private final PrintStream defaultStdout;
+  private boolean live;
+  private volatile boolean newEntry;
+
+  public StdConsoleHandler(final ByteArrayOutputStream buffer, final PrintStream defaultStdout) {
+    this.buffer = buffer;
+    this.defaultStdout = defaultStdout;
+    this.setDaemon(true);
+  }
+
+  @Override
+  public void run() {
+    live = true;
+    while (live) {
+      if (buffer.size() > 0) {
+        synchronized (buffer) {
+          if (newEntry) {
+            newEntry = false;
+            defaultStdout.println(":");
+          }
+          defaultStdout.print(Ansi.ansi().fg(Color.MAGENTA).a(buffer.toString()).reset());
+          buffer.reset();
+        }
+      }
+    }
+  }
+
+  public void terminate() {
+    live = false;
+  }
+
+  public boolean isNewEntry() {
+    return newEntry;
+  }
+
+  public void newEntry() {
+    synchronized (this) {
+      newEntry = true;
+    }
+  }
+}

879bb0a80671d54ac7fc30d276106b91b3646eb3
Vladyslav <ecstasy.awesome@gmail.com>
2021-03-19 12:34:17 +0200

    refactoring

--- src/main/java/academy/kovalevskyi/testing/service/ContainerRequest.java
+++ src/main/java/academy/kovalevskyi/testing/service/ContainerRequest.java
@@ -4,6 +4,7 @@ import academy.kovalevskyi.testing.exception.RequestException;
 import academy.kovalevskyi.testing.util.ContainerManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.Predicate;
 
 /**
@@ -11,9 +12,17 @@ import java.util.function.Predicate;
  */
 public class ContainerRequest implements Request {
 
+  private final String key;
+  private final int week;
+  private final int day;
+  private final int id;
   private final Predicate<Class<?>> predicate;
 
-  private ContainerRequest(Predicate<Class<?>> predicate) {
+  private ContainerRequest(String key, int week, int day, int id, Predicate<Class<?>> predicate) {
+    this.key = key;
+    this.week = week;
+    this.day = day;
+    this.id = id;
     this.predicate = predicate;
   }
 
@@ -22,11 +31,24 @@ public class ContainerRequest implements Request {
     return predicate;
   }
 
-  /**
-   * Builder of {@link ContainerRequest}.
-   *
-   * @return builder instance
-   */
+  @Override
+  public String toString() {
+    var message = new StringJoiner(", ", "[", "]");
+    if (key != null) {
+      message.add(String.format("key - %s", key));
+    }
+    if (week >= 0) {
+      message.add(String.format("week - %d", week));
+    }
+    if (day >= 0) {
+      message.add(String.format("day - %d", day));
+    }
+    if (id >= 0) {
+      message.add(String.format("id - %d", id));
+    }
+    return message.toString();
+  }
+
   public static Builder builder() {
     return new Builder();
   }
@@ -34,12 +56,15 @@ public class ContainerRequest implements Request {
   public static class Builder {
 
     private String key;
-    private boolean week;
-    private boolean day;
-    private boolean id;
+    private int week;
+    private int day;
+    private int id;
     private final List<Predicate<Class<?>>> predicates;
 
     private Builder() {
+      week = Integer.MIN_VALUE;
+      day = Integer.MIN_VALUE;
+      id = Integer.MIN_VALUE;
       predicates = new ArrayList<>();
     }
 
@@ -50,19 +75,19 @@ public class ContainerRequest implements Request {
     }
 
     public Builder week(int number) {
-      week = true;
+      week = number;
       predicates.add(clazz -> ContainerManager.getAnnotation(clazz).week() == number);
       return this;
     }
 
     public Builder day(int number) {
-      day = true;
+      day = number;
       predicates.add(clazz -> ContainerManager.getAnnotation(clazz).day() == number);
       return this;
     }
 
     public Builder container(int number) {
-      id = true;
+      id = number;
       predicates.add(clazz -> ContainerManager.getAnnotation(clazz).id() == number);
       return this;
     }
@@ -71,24 +96,21 @@ public class ContainerRequest implements Request {
      * Prepares request.
      *
      * @return instance of {@link ContainerRequest}
-     * @throws RuntimeException if request has some build errors
+     * @throws RequestException if request has some build errors
      */
     public ContainerRequest build() {
       if (key == null || key.isBlank()) {
         throw new RequestException("Course key should be provided");
       }
-      if (id && (!day || !week)) {
+      if (id >= 0 && (day < 0 || week < 0)) {
         throw new RequestException("Week and day should be provided to get some container");
       }
-      if (day && !week) {
+      if (day >= 0 && week < 0) {
         throw new RequestException("Week should be provided to get containers of some day");
       }
 
-      var predicate = predicates
-          .stream()
-          .reduce(entry -> true, Predicate::and);
-
-      return new ContainerRequest(predicate);
+      var predicate = predicates.stream().reduce(entry -> true, Predicate::and);
+      return new ContainerRequest(key, week, day, id, predicate);
     }
   }
 }

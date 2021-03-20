86072112d182e2268fc24ada5020c54be84fd180
Vladyslav <ecstasy.awesome@gmail.com>
2021-03-20 23:41:04 +0200

    improve ContainerTest

--- src/test/java/academy/kovalevskyi/testing/annotation/ContainerTest.java
+++ src/test/java/academy/kovalevskyi/testing/annotation/ContainerTest.java
@@ -1,8 +1,16 @@
 package academy.kovalevskyi.testing.annotation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import academy.kovalevskyi.testing.service.ContainerHandler;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 public class ContainerTest {
 
@@ -10,4 +18,21 @@ public class ContainerTest {
   public void testTypeIsAnnotation() {
     assertTrue(Container.class.isAnnotation());
   }
+
+  @Test
+  public void testAnnotationConfiguration() {
+    var retention = Container.class.getAnnotation(Retention.class);
+    var target = Container.class.getAnnotation(Target.class);
+    var extendWith = Container.class.getAnnotation(ExtendWith.class);
+
+    assertNotNull(retention);
+    assertNotNull(target);
+    assertNotNull(extendWith);
+    assertEquals(RetentionPolicy.RUNTIME, retention.value());
+    assertEquals(1, target.value().length);
+    assertEquals(ElementType.TYPE, target.value()[0]);
+    assertEquals(1, extendWith.value().length);
+    assertEquals(ContainerHandler.class, extendWith.value()[0]);
+  }
+
 }
